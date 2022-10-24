fbd518f58516d7f3cc0fcc308d6f885417d1901e
xin1126 <z26319491z@gmail.com>
2022-10-24 11:02:31 +0800

    fix: 圖片路徑引入

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col items-center text-5xl">
     <p>首頁</p>
-    <img class="w-1/4" src="../../assets/logo.png" alt="logo">
+    <img class="w-1/4" src="/images/logo.png" alt="logo">
   </div>
 </template>
